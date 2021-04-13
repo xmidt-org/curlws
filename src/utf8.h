d3f9bd4c12cfc03fbbe3d6991bde36e2ac77236f
Weston Schmidt <Weston_Schmidt@alumni.purdue.edu>
2021-04-13 02:41:44 -0700

    Remove the extra autogenerated header.

--- src/utf8.h
+++ src/utf8.h
@@ -1,7 +1,6 @@
 /*
  * Copyright (C) 2016 Gustavo Sverzut Barbieri
  * Copyright (c) 2021 Comcast Cable Communications Management, LLC
- * Copyright (c) 2021 Weston Schmidt
  *
  * Permission is hereby granted, free of charge, to any person
  * obtaining a copy of this software and associated documentation
@@ -24,23 +23,6 @@
  * SOFTWARE.
  *
  * https://opensource.org/licenses/MIT
- */
-/**
- *  Copyright (c) 2021 Comcast Cable Communications Management, LLC
- *  Copyright (c) 2021 Weston Schmidt
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
  */
 #ifndef __UTF8_H__
 #define __UTF8_H__
